88d25edc8e90200aca4e5321c32fea0ec416e70b
neTp9c <alexpetryakov@gmail.com>
2015-09-01 22:35:05 +0300

    add mediaItem alternate

---
+++ Handlers/ElementEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using Orchard.DisplayManagement;
+using Orchard.Environment.Extensions;
+using Orchard.Layouts.Elements;
+using Orchard.Layouts.Framework.Display;
+using Orchard.Layouts.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainBit.Layouts.Handlers
+{
+    public class ElementEventHandler : ElementEventHandlerBase
+    {
+        public override void Displayed(ElementDisplayedContext context)
+        {
+            if (context.Element is MediaItem)
+            {
+                (context.ElementShape as IShape).Metadata.OnDisplaying((displaying => {
+                    displaying.ShapeMetadata.Alternates.Add(String.Format("Elements_{0}_{1}", context.Element.GetType().Name, (context.Element as MediaItem).DisplayType));
+                }));
+            }
+        }
+    }
+}
