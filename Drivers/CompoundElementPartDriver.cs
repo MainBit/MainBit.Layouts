865c15e556de4baf014acc7effd49f134087ece6
neTp9c <alexpetryakov@gmail.com>
2016-01-17 14:57:14 +0300

    add export/import for compound part

--- Drivers/CompoundElementPartDriver.cs
+++ Drivers/CompoundElementPartDriver.cs
@@ -102,5 +102,27 @@ namespace MainBit.Layouts.Drivers {
                 return shapeHelper.EditorTemplate(TemplateName: "Parts/CompoundElement", Model: viewModel, Prefix: Prefix);
             });
         }
+
+        protected override void Exporting(CompoundElementPart part, ExportContentContext context)
+        {
+            context.Element(part.PartDefinition.Name).SetAttributeValue("ElementTypeName", part.ElementTypeName);
+            context.Element(part.PartDefinition.Name).SetAttributeValue("ElementDescription", part.ElementDescription);
+            context.Element(part.PartDefinition.Name).SetAttributeValue("ElementCategory", part.ElementCategory);
+        }
+
+        protected override void Importing(CompoundElementPart part, ImportContentContext context)
+        {
+            // Don't do anything if the tag is not specified.
+            if (context.Data.Element(part.PartDefinition.Name) == null)
+            {
+                return;
+            }
+
+            context.ImportAttribute(part.PartDefinition.Name, "ElementTypeName", x => part.ElementTypeName = x);
+            context.ImportAttribute(part.PartDefinition.Name, "ElementDescription", x => part.ElementDescription = x);
+            context.ImportAttribute(part.PartDefinition.Name, "ElementCategory", x => part.ElementCategory = x);
+        }
+
+        
     }
 }
